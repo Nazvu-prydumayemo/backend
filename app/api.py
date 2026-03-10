3d6f5edde0ba396a8f789de46d646200f25d005f
Hunter <dmytro.rebets@gmail.com>
2026-03-11 00:18:04 +0200

    NP-37 Create /auth Routes

--- app/api.py
+++ app/api.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 
+from app.features.auth.router import router as auth_router
 from app.features.ping.router import router as ping_router
+from app.features.user.router import router as user_router
 
 api_router = APIRouter()
+api_router.include_router(auth_router)
 api_router.include_router(ping_router)
+api_router.include_router(user_router)
