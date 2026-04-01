ba6a90ccb6b7176a9ac422675804375ff29d46a4
Hunter <dmytro.rebets@gmail.com>
2026-04-01 23:52:09 +0300

    NP-164 fix docs exposal

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 from app.api import api_router
+from app.core.config import settings
 
-app = FastAPI(title="NP-API")
+app = FastAPI(
+    title="NP-API", openapi_url=("/openapi.json" if settings.environment == "dev" else None)
+)
 
 app.include_router(api_router, prefix="/api/v1")
